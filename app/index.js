b24056a8805808c305657ee3c2d9c8595d7da6b9
PMJ <pmj@pmj.hu>
2019-07-28 19:22:18 +0200

    Clustered speedtest works

--- app/index.js
+++ app/index.js
@@ -5,6 +5,10 @@ const substrateMgr = require('./SubstrateManager');
 const test = new TestSuite();
 const os = require('os');
 const localKey = test.getKeypair(os.hostname());
+const { spawn } = require('child_process');
+const path = require('path');
+const Redis = require('ioredis');
+const redis = new Redis(6379, process.env.MASTER_IP || '127.0.0.1');
 
 process.on('SIGINT', () => {
     console.log('shutting down SUBSTRATE...');
@@ -30,9 +34,9 @@ nc.on('connect', function() {
     });
 
     //WHEN WEALTH SPREAD WAS DONE, WE SHOULD HAVE MONEY ON THIS ADDRESS
-    nc.subscribe('spread.done', function(amount) {
+    nc.subscribe('spread.done', function(balance) {
 
-        console.log('ittten', amount);
+        console.log('spread.done', balance);
         //BRING UP LOCAL SUBSTRATE NODE
         substrateMgr.up().then(nodeId => {
             console.log(nodeId);
@@ -45,7 +49,7 @@ nc.on('connect', function() {
             //INIT TEST SUITE (CONNECT TO LOCAL NODE RPC)
             test.init().then(instance => {
 
-                instance.queryBalanceUntil(localKey.address, amount, (isBalanceOK) => {
+                instance.queryBalanceUntil(localKey.address, balance, (isBalanceOK) => {
 
                     if(!isBalanceOK){
                         process.stderr.write(`BALANCE NOT FOUND FOR ADDRESS ${localKey.address}` + "\n");
@@ -56,15 +60,45 @@ nc.on('connect', function() {
                     nc.publish('node.balanceOK', {
                         address: localKey.address,
                         totalNodes: process.env.TOTAL_NODES || 1,
-                        balance: amount
+                        balance: balance
                     });
-                }, 60 * 1000);
+                }, null, 60 * 1000);
+
+                //WHEN WEALTH SPREAD WAS DONE, WE SHOULD HAVE MONEY ON THIS ADDRESS
+                nc.subscribe('speedtest.start', function() {
+                    console.log('szpidteszt on!');
+
+                    const clustered = spawn('node', [ __dirname + path.sep + 'speedtest.js' ]);
+
+                    clustered.stdout.on('data', (data) => {
+                        console.log(`${data}`.trim());
+                    });
+
+                    clustered.stderr.on('data', (data) => {
+                        console.log(`STDERR: ${data}`);
+                    });
+
+                    clustered.on('close', (code) => {
+                        console.log(`child process exited with code ${code}`);
+                    });
+
+                    //SUBSCRIBE IN REDIS FOR THE TX CLUSTER FINISH
+                    redis.subscribe('cluster.done', function() {
+
+                        //UPON RECEIVING THIS EVENT
+                        redis.on("message", function(channel, message) {
+                            if(channel === 'cluster.done'){
+
+                                //PUBLISH IN NATS THAT THIS NODE HAS DONE TESTING
+                                nc.publish('speedtest.finished', {
+                                    address: localKey.address,
+                                    totalNodes: process.env.TOTAL_NODES || 1
+                                });
+                            }
+                        });
+                    });
+                });
             });
         });
-    });
-
-    //WHEN WEALTH SPREAD WAS DONE, WE SHOULD HAVE MONEY ON THIS ADDRESS
-    nc.subscribe('speedtest.start', function() {
-        console.log('RUN DA TEST!!!!!');
     });
 });
