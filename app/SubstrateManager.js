af675618e4ba046b91f82a2c37ed77d19475d3b8
PMJ <pmj@pmj.hu>
2019-07-28 14:16:57 +0200

    Modularize bootnode args

--- app/SubstrateManager.js
+++ app/SubstrateManager.js
@@ -1,10 +1,15 @@
 const { spawn, exec } = require('child_process');
+const bootnodeArgs = {
+    ip: process.env.BOOTNODE_IP || process.env.MASTER_IP || '172.28.1.1',
+    port: process.env.BOOTNODE_PORT || 30333,
+    id: process.env.BOOTNODE_ID || 'QmRpheLN4JWdAnY7HGJfWFNbfkQCb6tFf4vvA6hgjMZKrR'
+}
 const spawnArgs = [
     '--chain=local',
     '-d',
     '/data',
     '--bootnodes',
-    process.env.BOOTNODE
+    `/ip4/${bootnodeArgs.ip}/tcp/${bootnodeArgs.port}/p2p/${bootnodeArgs.id}`
 ];
 
 
@@ -23,7 +28,7 @@ module.exports = {
 
             //TRY TO PARSE LOCAL NODE ADDR. FROM STDOUT
             substrate.stdout.on('data', (data) => {
-
+                console.log(data.toString());
                 const nodeId = findNodeId(data.toString());
                 if(nodeId !== false){
                     resolve(nodeId);
