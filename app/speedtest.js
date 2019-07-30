f4f19aeee62d6c9912a4dc48bf043be441e10d10
PMJ <pmj@pmj.hu>
2019-07-30 14:08:55 +0200

    Fix nonce increment

--- app/speedtest.js
+++ app/speedtest.js
@@ -15,16 +15,15 @@ const provider = new WsProvider('ws://127.0.0.1:9944');
 
 ApiPromise.create(provider).then(api => {
 
-    api.query.system.accountNonce(localKey.address).then(rawNonce1 => {
+    api.query.system.accountNonce(localKey.address).then(rawNonce => {
 
-        let nonce1 = new BN(rawNonce1.toString());
+        let nonce = new BN(rawNonce.toString());
         for (let i = 0; i < 100; i++) {
             const transfer = api.tx.balances.transfer(test.accounts[0], 500);
-            let nonce = nonce1;
             transfer.signAndSend(localKey, {
                 nonce
             });
-            nonce1 = nonce1.add(new BN(1));
+            nonce = nonce.add(new BN(1));
         }
         process.stdout.write('FINISHED');
         setTimeout(() => {
